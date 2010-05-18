10b4aa0250609eaf77586b2a49121b233b3856ac
pelya <x.pelya.x@gmail.com>
2010-05-18 16:18:11 +0300

    Added sdl_ttf and freetype libs

--- alienblaster/Application.mk
+++ alienblaster/Application.mk
@@ -1,2 +1,2 @@
 APP_PROJECT_PATH := $(call my-dir)/project
-APP_MODULES      := application stlport sdl sdl_main sdl_mixer tremor sdl_image png jpeg
+APP_MODULES      := application stlport sdl sdl_main sdl_mixer tremor sdl_image png jpeg sdl_ttf freetype
