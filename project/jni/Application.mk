1ef6967d7de060612e45fd2b61dcaf6fedaf8be1
pelya <x.pelya.x@gmail.com>
2010-09-03 18:43:17 +0300

    Changed default app to Tyrian

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -5,6 +5,6 @@ APP_PROJECT_PATH := $(call my-dir)/..
 # sdl_image depends on png and jpeg
 # sdl_ttf depends on freetype
 
-APP_MODULES := application sdl sdl_main stlport tremor png jpeg freetype sdl_image sdl_mixer
+APP_MODULES := application sdl sdl_main stlport tremor png jpeg freetype sdl_net
 
 APP_ABI := armeabi
