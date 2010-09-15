379538c2e3bdf338acc371360d53a8b943ec97bb
pelya <x.pelya.x@gmail.com>
2010-09-15 17:01:29 +0300

    Reverted the CrystaX toolchain hardcoded path in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Set here your own NDK path if needed
-# export PATH=$PATH:~/src/endless_space/android-ndk-r4
+# export PATH=$PATH:~/src/endless_space/android-ndk-r4b
+# Set environment to CrystaX NDK with RTTI and exceptions instead of original NDK
+# export PATH=$PATH:~/src/endless_space/android-ndk-r4-crystax/ndk-build
 
-cd project && nice -n5 ~/src/endless_space/android-ndk-r4-crystax/ndk-build -j2 V=1 && ant debug && cd bin && adb install -r DemoActivity-debug.apk
+cd project && nice -n5 ndk-build -j2 V=1 && ant debug && cd bin && adb install -r DemoActivity-debug.apk
 
