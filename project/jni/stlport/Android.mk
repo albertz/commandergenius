b241a0de47d062a8354d102928967f46ef9372af
pelya <x.pelya.x@gmail.com>
2010-09-16 15:25:20 +0300

    Build error in stlport

--- project/jni/stlport/Android.mk
+++ project/jni/stlport/Android.mk
@@ -10,7 +10,7 @@ LOCAL_SRC_FILES := dummy.c
 else
 LOCAL_CFLAGS := -I$(LOCAL_PATH)/stlport -I$(LOCAL_PATH)/src
 LOCAL_CPP_EXTENSION := .cpp
-LOCAL_SRC_FILES := $(addprefix src/,$(notdir $(wildcard $(LOCAL_PATH)/src/*.cpp $(LOCAL_PATH)/src/*.c))))
+LOCAL_SRC_FILES := $(addprefix src/,$(notdir $(wildcard $(LOCAL_PATH)/src/*.cpp $(LOCAL_PATH)/src/*.c)))
 endif
 
 include $(BUILD_STATIC_LIBRARY)
