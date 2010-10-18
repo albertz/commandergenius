73623a740e9cec4447f9a7223f13d4920729bf99
pelya <x.pelya.x@gmail.com>
2010-10-18 16:30:51 +0300

    Checking that my accelerometer changes work okay in both joystick and keyboard mode

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -2,7 +2,7 @@ APP_PROJECT_PATH := $(call my-dir)/..
 
 # Available libraries: mad sdl_mixer sdl_image sdl_ttf sdl_net sdl_blitpool sdl_gfx intl xml2 lua jpeg png tremor freetype xerces
 
-APP_MODULES := application sdl-1.3 sdl_main stlport tremor png jpeg freetype xerces sdl_mixer sdl_image
+APP_MODULES := application sdl-1.2 sdl_main stlport tremor png jpeg freetype xerces sdl_mixer sdl_image sdl_ttf intl lua png xerces
 
 # To filter out static libs from all libs in makefile
 APP_AVAILABLE_STATIC_LIBS := jpeg png tremor freetype xerces
