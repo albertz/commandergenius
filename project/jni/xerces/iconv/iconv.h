23575576cacfe27f0dcd4440cf1ec507fc24b7c7
pelya <x.pelya.x@gmail.com>
2010-10-19 17:14:30 +0300

    xerces does not depend on SDL, so not rebuilt when libSDL version changes

--- project/jni/xerces/iconv/iconv.h
+++ project/jni/xerces/iconv/iconv.h
@@ -7,7 +7,15 @@
 extern "C" {
 #endif
 
-#include <SDL.h>
+// #include <SDL.h> // We'll have to rebuild xerces if we'll include this file and change SDL version, so just provide declarations here
+
+typedef struct _SDL_iconv_t *SDL_iconv_t;
+
+extern SDL_iconv_t SDL_iconv_open(const char *tocode, const char *fromcode);
+extern int SDL_iconv_close(SDL_iconv_t cd);
+extern size_t SDL_iconv(SDL_iconv_t cd, const char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft);
+extern char * SDL_iconv_string(const char *tocode, const char *fromcode, const char *inbuf, size_t inbytesleft);
+
 #define iconv_t SDL_iconv_t
 #define iconv SDL_iconv
 #define iconv_open SDL_iconv_open
