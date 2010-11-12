b9e424ea10efaa1ff78cc30fd5159d432ac3a5aa
pelya <x.pelya.x@gmail.com>
2010-11-12 11:58:53 +0200

    Added mikmod library, also added it to SDL_mixer

---
+++ project/jni/mikmod/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mikmod
+
+APP_SUBDIRS := $(patsubst $(LOCAL_PATH)/%, %, $(shell find $(LOCAL_PATH) -type d))
+
+LOCAL_CFLAGS := -O3 $(foreach D, $(APP_SUBDIRS), -I$(LOCAL_PATH)/$(D)) \
+				-I$(LOCAL_PATH)/include -I$(LOCAL_PATH) -DHAVE_CONFIG_H
+
+
+LOCAL_CPP_EXTENSION := .cpp
+
+LOCAL_SRC_FILES := $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.cpp))))
+LOCAL_SRC_FILES += $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.c))))
+
+LOCAL_SHARED_LIBRARIES := 
+
+LOCAL_STATIC_LIBRARIES := 
+
+LOCAL_LDLIBS :=
+
+include $(BUILD_STATIC_LIBRARY)
--- project/jni/sdl_mixer/Android.mk
+++ project/jni/sdl_mixer/Android.mk
@@ -5,15 +5,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sdl_mixer
 
 LOCAL_CFLAGS := -O3 -I$(LOCAL_PATH) -I$(LOCAL_PATH)/../sdl-$(SDL_VERSION)/include -I$(LOCAL_PATH)/include \
-				-I$(LOCAL_PATH)/../mad/include -I$(LOCAL_PATH)/../flac/include -I$(LOCAL_PATH)/../ogg/include -I$(LOCAL_PATH)/../vorbis/include -I$(LOCAL_PATH)/../tremor/include \
-					-DWAV_MUSIC -DOGG_USE_TREMOR -DOGG_MUSIC -DFLAC_MUSIC
+				-I$(LOCAL_PATH)/../mad/include -I$(LOCAL_PATH)/../flac/include -I$(LOCAL_PATH)/../ogg/include \
+				-I$(LOCAL_PATH)/../vorbis/include -I$(LOCAL_PATH)/../tremor/include -I$(LOCAL_PATH)/../mikmod/include \
+					-DWAV_MUSIC -DOGG_USE_TREMOR -DOGG_MUSIC -DFLAC_MUSIC -DMOD_MUSIC
 
 LOCAL_CPP_EXTENSION := .cpp
 
 LOCAL_SRC_FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
 
 LOCAL_SHARED_LIBRARIES := sdl-$(SDL_VERSION)
-LOCAL_STATIC_LIBRARIES := flac
+LOCAL_STATIC_LIBRARIES := flac mikmod
 
 ifeq "$(TARGET_ARCH_ABI)" "armeabi"
 LOCAL_CFLAGS += -DOGG_USE_TREMOR
