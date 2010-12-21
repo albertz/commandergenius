87bae09b46846bc976aa89b88ba210c2c8153ff8
Jerome Marty <jahrome11@gmail.com>
2010-12-21 13:17:30 +0100

    Updated ltris build script

--- project/jni/application/ltris-1.0.15/AndroidBuild.sh
+++ project/jni/application/ltris-1.0.15/AndroidBuild.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+if [ -e $LOCAL_PATH/../../../obj/local/armeabi/libsdl_mixer.so ] ; then
+	ln -sf $LOCAL_PATH/../../../obj/local/armeabi/libsdl_mixer.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_mixer.so
+fi
+
 if [ \! -f Makefile ] ; then
 	../launchConfigure.sh --disable-install --enable-sdl-net LIBS=-lintl
 fi
