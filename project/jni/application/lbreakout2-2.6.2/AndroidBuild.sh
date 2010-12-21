3e76f89c17d2296246133a35146c7f2d8f62825c
Jerome Marty <jahrome11@gmail.com>
2010-12-21 13:17:48 +0100

    Updated lbreakout2 build script

--- project/jni/application/lbreakout2-2.6.2/AndroidBuild.sh
+++ project/jni/application/lbreakout2-2.6.2/AndroidBuild.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+if [ -e $LOCAL_PATH/../../../obj/local/armeabi/libsdl_mixer.so ] ; then
+	ln -sf $LOCAL_PATH/../../../obj/local/armeabi/libsdl_mixer.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_mixer.so
+fi
+
+if [ -e $LOCAL_PATH/../../../obj/local/armeabi/libsdl_net.so ] ; then
+	ln -sf $LOCAL_PATH/../../../obj/local/armeabi/libsdl_net.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_net.so
+fi
+
 if [ \! -f Makefile ] ; then
 	../launchConfigure.sh --disable-install --enable-sdl-net LIBS=-lintl
 fi
