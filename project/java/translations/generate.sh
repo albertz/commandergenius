c0c7eaa5dabdb4f2b53300572f9ebd8fc3d554d4
pelya <x.pelya.x@gmail.com>
2010-12-22 17:22:25 +0000

    Updated translation script

--- project/java/translations/generate.sh
+++ project/java/translations/generate.sh
@@ -19,8 +19,7 @@ grep '<string name=' values/strings.xml | while read str; do
 			echo "<string name=\"$var\">$transtext</string>" >> $dir/strings.1.xml
 			echo "</resources>" >> $dir/strings.1.xml
 			mv -f $dir/strings.1.xml $dir/strings.xml
-			lang=`echo $lang | tr '[a-z]' '[A-Z]'`
+			echo $lang: $var: $transtext
 		fi
-		echo $lang: $var: $transtext
 	done
 done
