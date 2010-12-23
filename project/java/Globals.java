1f23523ddb8b61eee10fd068bf71a2f6fa0d45f1
pelya <x.pelya.x@gmail.com>
2010-12-23 09:14:09 +0000

    Small update to on-screen keyboaqrd defaults

--- project/java/Globals.java
+++ project/java/Globals.java
@@ -53,7 +53,7 @@ class Globals {
 	public static boolean PhoneHasArrowKeys = false;
 	public static boolean UseAccelerometerAsArrowKeys = false;
 	public static boolean UseTouchscreenKeyboard = true;
-	public static int TouchscreenKeyboardSize = 0;
+	public static int TouchscreenKeyboardSize = 1;
 	public static int TouchscreenKeyboardTheme = 2;
 	public static int TouchscreenKeyboardTransparency = 2;
 	public static int AccelerometerSensitivity = 2;
