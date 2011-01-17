9de018c3d8d14881abd24734796bcb5ba233c1f7
pelya <x.pelya.x@gmail.com>
2011-01-17 15:58:10 +0000

    Script to check static data size in a libapplication.so

---
+++ checkStaticDataSize.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Output static/global symbols in a application lib to check if someone allocated 200 Mb static temp array.
+# Used to resolve linker errors such as: Cannot load library: alloc_mem_region[815]: OOPS:    54 cannot map library 'libapplication.so'. no vspace available.
+objdump -x project/obj/local/armeabi/libapplication.so | grep '	' | sed 's/.*	/0x/' | sort -n
