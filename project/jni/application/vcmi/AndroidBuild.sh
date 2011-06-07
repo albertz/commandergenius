dd2e589159cc25a54b4c1b9633a08255642c8de0
pelya <x.pelya.x@gmail.com>
2011-06-07 13:49:57 +0300

    VCMI compiles, but does not work, because the server and AI libraries should be linked statically to it

---
+++ project/jni/application/vcmi/AndroidBuild.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+# Uncomment if your configure expects SDL libraries in form "libSDL_name.so"
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL.so
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi/libpthread.so
+ln -sf libsdl_mixer.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_mixer.so
+ln -sf libsdl_image.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_image.so
+ln -sf libsdl_ttf.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_ttf.so
+
+if [ \! -f vcmi/Makefile -o $0 -nt vcmi/Makefile ] ; then
+	../setEnvironment.sh sh -c "cd vcmi && \
+	env LIBS='-lavcodec -lavutil -lavcore -lgcc' \
+	./configure --host=arm-eabi --enable-static \
+	--with-boost-system=boost_system \
+	--with-boost-filesystem=boost_filesystem \
+	--with-boost-thread=boost_thread \
+	--with-boost-iostreams=boost_iostreams \
+	--with-boost-program-options=boost_program_options" || { rm -f libapplication.so ; exit 1 ; }
+fi
+../setEnvironment.sh sh -c "cd vcmi && \
+  env pkgdatadir=. \
+  AM_MAKEFLAGS=pkgdatadir=. \
+  make -j4 VERBOSE=1 pkgdatadir=. AM_MAKEFLAGS=pkgdatadir=." && \
+cp -f vcmi/client/vcmiclient libapplication.so
+
+rm -f $LOCAL_PATH/../../../obj/local/armeabi/libSDL.so
+rm -f $LOCAL_PATH/../../../obj/local/armeabi/libpthread.so
+rm -f $LOCAL_PATH/../../../obj/local/armeabi/libSDL_mixer.so
+rm -f $LOCAL_PATH/../../../obj/local/armeabi/libSDL_image.so
+rm -f $LOCAL_PATH/../../../obj/local/armeabi/libSDL_ttf.so
