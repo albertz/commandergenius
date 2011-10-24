5ca1cf38ee19e164dc1945bd81c59e04c5e5a659
pelya <x.pelya.x@gmail.com>
2011-10-24 12:47:17 +0300

    Added PrefClub project (a small Dos game wrapped in Dosbox)

---
+++ project/jni/application/prefclub/AndroidBuild.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL.so
+ln -sf libsdl_net.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_net.so
+ln -sf libsdl_sound.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_sound.so
+
+if [ \! -f dosbox-0.74/configure ] ; then
+	sh -c "cd dosbox-0.74 && ./autogen.sh"
+fi
+
+if [ \! -f dosbox-0.74/Makefile ] ; then
+	../setEnvironment.sh sh -c "cd dosbox-0.74 && ./configure --build=x86_64-unknown-linux-gnu --host=arm-eabi"
+fi
+
+../setEnvironment.sh sh -c "cd dosbox-0.74 && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -lpng -lgcc -lz -lc -lstdc++'" && cp -f dosbox-0.74/src/dosbox libapplication.so
