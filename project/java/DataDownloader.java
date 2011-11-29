9ed305398c6e06d3f5c0fdca995c9f2de4233087
pelya <x.pelya.x@gmail.com>
2011-11-29 17:48:03 +0200

    Fixed progress message in data downloader

--- project/java/DataDownloader.java
+++ project/java/DataDownloader.java
@@ -199,7 +199,7 @@ class DataDownloader extends Thread
 			if( downloadFiles[i].length() > 0 &&
 				( Globals.OptionalDataDownload.length > i && Globals.OptionalDataDownload[i] ) ||
 				( Globals.OptionalDataDownload.length <= i && downloadFiles[i].indexOf("!") == 0 ) )
-				if( ! DownloadDataFile(downloadFiles[i], "libsdl-DownloadFinished-" + String.valueOf(i) + ".flag", count, total) )
+				if( ! DownloadDataFile(downloadFiles[i], "libsdl-DownloadFinished-" + String.valueOf(i) + ".flag", count+1, total) )
 				{
 					DownloadFailed = true;
 					return;
