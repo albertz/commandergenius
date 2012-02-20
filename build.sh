c746cbbecfad228d1f93524e6de668faa5d5183e
pelya <x.pelya.x@gmail.com>
2012-02-20 20:50:58 +0200

    Another fix to my prev commit

--- build.sh
+++ build.sh
@@ -37,6 +37,6 @@ cd project && env PATH=$NDKBUILDPATH nice -n19 ndk-build V=1 -j4 && \
    || true ; } && \
  ant debug && \
  test -z "$1" && cd bin && \
- adb install -r MainActivity-debug.apk | grep 'Failure' && \
- adb uninstall `grep AppFullName ../../AndroidAppSettings.cfg | sed 's/.*=//'` && adb install -r MainActivity-debug.apk && \
+ { adb install -r MainActivity-debug.apk | grep 'Failure' && \
+ adb uninstall `grep AppFullName ../../AndroidAppSettings.cfg | sed 's/.*=//'` && adb install -r MainActivity-debug.apk ; true ; } && \
  adb shell am start -n `grep AppFullName ../../AndroidAppSettings.cfg | sed 's/.*=//'`/.MainActivity
