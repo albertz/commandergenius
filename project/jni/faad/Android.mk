f0bfa0e71f5be74e35230d11004a1fb073c14ee8
pelya <x.pelya.x@gmail.com>
2012-02-29 17:13:47 +0200

    Added libfaad (thanks to Lubomyr)

---
+++ project/jni/faad/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := faad
+
+APP_SUBDIRS := $(patsubst $(LOCAL_PATH)/%, %, $(shell find $(LOCAL_PATH)/src -type d))
+
+LOCAL_C_INCLUDES := $(foreach D, $(APP_SUBDIRS), $(LOCAL_PATH)/$(D)) $(LOCAL_PATH)/include $(LOCAL_PATH)/../sdl-$(SDL_VERSION)/include
+LOCAL_CFLAGS := -O3 -DHAVE_CONFIG_H -include stdint.h -Werror=implicit
+
+LOCAL_CPP_EXTENSION := .cpp
+
+LOCAL_SRC_FILES := $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.cpp))))
+LOCAL_SRC_FILES += $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.c))))
+
+LOCAL_SHARED_LIBRARIES := sdl-$(SDL_VERSION)
+
+LOCAL_STATIC_LIBRARIES := 
+
+LOCAL_LDLIBS := 
+
+include $(BUILD_SHARED_LIBRARY)
