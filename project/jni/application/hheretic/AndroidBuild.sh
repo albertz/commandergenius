6c03bdc131323af90d1c11ffb0c33ccd35639df0
pelya <x.pelya.x@gmail.com>
2012-06-11 17:36:07 +0300

    Updated hHeretic, this is not yet usable because there's no button to change weapons

--- project/jni/application/hheretic/AndroidBuild.sh
+++ project/jni/application/hheretic/AndroidBuild.sh
@@ -5,10 +5,11 @@ LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 ln -sf libsdl_mixer.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_mixer.so
 
+[ -e hheretic-0.2.2-src ] || curl --location "http://sourceforge.net/project/downloading.php?group_id=4501&filename=hheretic-0.2.2-src.tgz" | gunzip | tar xv
 cd hheretic-0.2.2-src
 if [ \! -f Makefile ] ; then
-	../../launchConfigure.sh --disable-gl
+	../../setEnvironment.sh ./configure --host=arm-linux-androideabi --disable-gl --without-x
 fi
 rm -f ../libapplication.so
-make
+make -j4 CPPFLAGS="-include SDL_main.h -Iinclude -I."
 mv -f hheretic-sdl ../libapplication.so
