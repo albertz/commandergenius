ad6cd2c92e8af8e6b1d6e5d3016e3c980afa0198
pelya <x.pelya.x@gmail.com>
2012-11-26 19:46:42 +0200

    Script for creating .tar.gz source archive, with whole SDL repo plus app sources

---
+++ createSourceArchive.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+APPNAME=`grep AppName AndroidAppSettings.cfg | sed 's/.*=//' | tr -d '"' | tr " '/" '---'`
+APPVER=`grep AppVersionName AndroidAppSettings.cfg | sed 's/.*=//' | tr -d '"' | tr " '/" '---'`
+
+# TODO: Boost and Python are stored in repository as precompiled binaries, the proper way to fix that is to build them using scripts, and remove that binaries
+tar -c -z --exclude-vcs --exclude="*.o" --exclude="*.a" --exclude="*.so" --exclude="*.d" --exclude="*.dep" \
+-f $APPNAME-$APPVER-src.tar.gz \
+`git ls-files --exclude-standard | grep -v '^project/jni/application/.*'` \
+`find  project/jni/application -maxdepth 1 -type f` \
+project/jni/application/src \
+project/jni/application/`readlink project/jni/application/src` \
+project/AndroidManifest.xml project/src
