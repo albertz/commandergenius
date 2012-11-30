95562a142175aaa9720363adfa271921c2798312
pelya <x.pelya.x@gmail.com>
2012-11-30 03:11:17 +0200

    Fixed build scripts

--- build.sh
+++ build.sh
@@ -15,6 +15,10 @@ if [ "$#" -gt 0 -a "$1" = "-r" ]; then
 	run_apk=true
 fi
 
+[ -e local.properties ] || {
+	android update project -p project || exit 1
+	rm -f project/src/Globals.java
+}
 # Set here your own NDK path if needed
 # export PATH=$PATH:~/src/endless_space/android-ndk-r7
 NDKBUILDPATH=$PATH
