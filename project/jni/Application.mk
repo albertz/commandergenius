b4c9197b5512760205fd9d4bbe5973d2b00602ca
pelya <x.pelya.x@gmail.com>
2013-01-06 01:58:59 +0200

    Changed NDK target platform to Android 4.0, maybe this will fix misaligned pointers bugs

--- project/jni/Application.mk
+++ project/jni/Application.mk
@@ -2,7 +2,7 @@ APP_PROJECT_PATH := $(call my-dir)/..
 
 APP_STL := gnustl_static
 APP_CFLAGS := -O3 -DNDEBUG -g # arm-linux-androideabi-4.4.3 crashes in -O0 mode on SDL sources
-APP_PLATFORM := android-9 # Android 2.3.0 or newer, includes EGL headers
+APP_PLATFORM := android-14 # Android 4.0, it should be backward compatible to previous versions
 
 include jni/Settings.mk
 
