da0d0826f859af3d7d89c3e7a685149e00572f12
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-05-29 23:18:44 +0300

    Initial port of UQM-HD

---
+++ project/jni/application/uqm-hd/AndroidPreBuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+if [ -e src/patched.successfully ]; then
+	exit 0
+else
+	svn co http://uqm-hd.googlecode.com/svn/trunk/src && patch -p0 -d src < android.diff && touch src/patched.successfully || exit 1
+fi
