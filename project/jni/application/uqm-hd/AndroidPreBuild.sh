d97349e8c170bba265df1fc7a236196733567673
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-06-11 19:37:20 +0300

    UQM-HD: directional joystick option

--- project/jni/application/uqm-hd/AndroidPreBuild.sh
+++ project/jni/application/uqm-hd/AndroidPreBuild.sh
@@ -6,5 +6,5 @@ LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 if [ -e src/patched.successfully ]; then
 	exit 0
 else
-	svn co http://uqm-hd.googlecode.com/svn/trunk/src && patch -p0 < android.diff && touch src/patched.successfully || exit 1
+	svn co http://uqm-hd.googlecode.com/svn/trunk/src && patch -p0 < android.diff && svn add src/config_unix.h && touch src/patched.successfully || exit 1
 fi
