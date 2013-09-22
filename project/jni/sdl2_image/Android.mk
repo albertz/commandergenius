1b194b4cbb18708608d173e9dd6fda5553d43352
Gerhard Stein <gerstrong@gmail.com>
2013-09-22 19:24:32 +0200

    Updated libpng to version 16 and added sdl2_image for sdl2

---
+++ project/jni/sdl2_image/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := sdl2_image
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/../jpeg/include $(LOCAL_PATH)/../png/include $(LOCAL_PATH)/../sdl-$(SDL_VERSION)/include $(LOCAL_PATH)/include
+LOCAL_CFLAGS := -O3 \
+       -DLOAD_JPG -DLOAD_PNG -DLOAD_BMP -DLOAD_GIF -DLOAD_LBM \
+       -DLOAD_PCX -DLOAD_PNM -DLOAD_TGA -DLOAD_XCF -DLOAD_XPM \
+       -DLOAD_XV
+
+LOCAL_CPP_EXTENSION := .cpp
+
+LOCAL_SRC_FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
+
+LOCAL_STATIC_LIBRARIES := png jpeg
+
+LOCAL_SHARED_LIBRARIES := sdl-$(SDL_VERSION)
+
+LOCAL_LDLIBS := -lz
+
+include $(BUILD_SHARED_LIBRARY)
+
