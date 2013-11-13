2fb6a5bd951fdec538eb689f2e4d1375fe3f1664
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-11-13 22:33:01 +0200

    Xserver-gimp: better algorithm for finding stale Xserver sockets

--- project/jni/application/xserver/main.c
+++ project/jni/application/xserver/main.c
@@ -95,6 +95,25 @@ int main( int argc, char* argv[] )
 			close(s);
 		}
 		*/
+		FILE * ff = fopen("/proc/net/unix", "rb");
+		if( ff )
+		{
+			char buf[512], name[512];
+			int found = 0;
+			sprintf(name, "/tmp/.X11-unix/X%d", i);
+			while( fgets(buf, sizeof(buf), ff) )
+			{
+				if( strstr(buf, name) != NULL )
+				{
+					__android_log_print(ANDROID_LOG_INFO, "XSDL", "UNIX path %s already used, trying next one", name);
+					found = 1;
+					break;
+				}
+			}
+			fclose(ff);
+			if( found )
+				continue;
+		}
 
 		sprintf( port, ":%d", i );
 		break;
