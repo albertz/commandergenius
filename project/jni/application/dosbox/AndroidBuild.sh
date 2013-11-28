3d103698c7baca414c8e55c814ea57a10fa3cc3e
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-11-28 20:23:19 +0200

    Updated Dosbox to compile for all architectures

--- project/jni/application/dosbox/AndroidBuild.sh
+++ project/jni/application/dosbox/AndroidBuild.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
+ln -sf libsdl-1.2.so ../../../obj/local/$1/libSDL.so
+ln -sf libsdl_net.so ../../../obj/local/$1/libSDL_net.so
+ln -sf libsdl_sound.so ../../../obj/local/$1/libSDL_sound.so
 
-LOCAL_PATH=`dirname $0`
-LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+[ -e $1/dosbox-0.74 ] || {
+	mkdir -p $1
+	[ -e dosbox-0.74.tar.gz ] || wget http://sourceforge.net/projects/dosbox/files/dosbox/0.74/dosbox-0.74.tar.gz || exit 1
+	cd $1 && tar xvf ../dosbox-0.74.tar.gz && patch -p0 < ../dosbox-0.74-androidSDL.diff && cd .. || exit 1
+} || exit 1
 
-ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL.so
-ln -sf libsdl_net.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_net.so
-ln -sf libsdl_sound.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL_sound.so
-
-if [ \! -d dosbox-0.74 ] ; then
-	wget http://sourceforge.net/projects/dosbox/files/dosbox/0.74/dosbox-0.74.tar.gz && tar xvf dosbox-0.74.tar.gz && patch -p0 < dosbox-0.74-androidSDL.diff || exit 1
-fi
-
-if [ \! -f dosbox-0.74/configure ] ; then
-	sh -c "cd dosbox-0.74 && ./autogen.sh" || exit 1
-fi
-
-if [ \! -f dosbox-0.74/Makefile ] ; then
+[ -e $1/dosbox-0.74/Makefile ] || {
+	sh -c "cd $1/dosbox-0.74 && ./autogen.sh" || exit 1
 	env CFLAGS="-frtti -fexceptions" LDFLAGS="-frtti -fexceptions" \
-	../setEnvironment.sh sh -c "cd dosbox-0.74 && ./configure --build=x86_64-unknown-linux-gnu --host=arm-linux-androideabi" || exit 1
-fi
+	../setEnvironment-$1.sh sh -c "cd $1/dosbox-0.74 && ./configure --build=x86_64-unknown-linux-gnu --host=$2" || exit 1
+} || exit 1
 
-../setEnvironment.sh sh -c "cd dosbox-0.74 && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -lpng -lgcc -lz -lc -lgnustl_static -lsupc++'" && cp -f dosbox-0.74/src/dosbox libapplication.so || exit 1
+../setEnvironment-$1.sh sh -c "cd $1/dosbox-0.74 && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -lpng -lgcc -lz -lc -lgnustl_static -lsupc++'" && cp -f $1/dosbox-0.74/src/dosbox libapplication-$1.so || exit 1
 exit 0
