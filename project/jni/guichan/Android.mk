7f0c344e5aea4c40a113dfaa3e298420f83084fd
pelya <x.pelya.x@gmail.com>
2013-12-03 22:20:34 +0200

    Added TTF support to Guichan library

--- project/jni/guichan/Android.mk
+++ project/jni/guichan/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE := guichan
 APP_SUBDIRS := $(patsubst $(LOCAL_PATH)/%, %, $(shell find $(LOCAL_PATH)/src -type d))
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/../sdl-$(SDL_VERSION)/include $(LOCAL_PATH)/include \
-					$(LOCAL_PATH)/../sdl_image/include
+					$(LOCAL_PATH)/../sdl_image/include $(LOCAL_PATH)/../sdl_ttf/include
 LOCAL_CFLAGS := -DHAVE_CONFIG_H -D_GNU_SOURCE=1 -D_REENTRANT -fexceptions -frtti
 
 LOCAL_CPP_EXTENSION := .cpp
@@ -16,7 +16,7 @@ LOCAL_SRC_FILES := $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wil
 LOCAL_SRC_FILES += $(foreach F, $(APP_SUBDIRS), $(addprefix $(F)/,$(notdir $(wildcard $(LOCAL_PATH)/$(F)/*.c))))
 
 LOCAL_SHARED_LIBRARIES := sdl-$(SDL_VERSION)
-LOCAL_SHARED_LIBRARIES += sdl_image
+LOCAL_SHARED_LIBRARIES += sdl_image sdl_ttf
 
 LOCAL_STATIC_LIBRARIES := 
 
