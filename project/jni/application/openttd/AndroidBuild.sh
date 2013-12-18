8d328c39d53efcecffe44621c4ac28c0495441f1
Sergii Pylypenko <x.pelya.x@gmail.com>
2013-12-18 21:08:17 +0200

    Updated OpenTTD

--- project/jni/application/openttd/AndroidBuild.sh
+++ project/jni/application/openttd/AndroidBuild.sh
@@ -2,21 +2,22 @@
 
 LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
-VER=1.3.2
+VER=1.3.3
 
-
-if [ \! -d openttd-$VER ] ; then
-	wget http://binaries.openttd.org/releases/$VER/openttd-$VER-source.tar.gz -O - | tar xvz && \
-	cp -a -f openttd-$VER openttd-$VER-orig && \
-	patch -p 0 -d openttd-$VER < openttd-trunk-android.patch && patch -p 0 -d openttd-$VER < findversion.sh.patch || exit 1
+if [ \! -d openttd-$VER-$1 ] ; then
+	[ -e openttd-$VER-source.tar.gz ] || wget http://binaries.openttd.org/releases/$VER/openttd-$VER-source.tar.gz || exit 1
+	tar xvzf openttd-$VER-source.tar.gz
+	cp -a -f openttd-$VER openttd-$VER-orig
+	mv -f openttd-$VER openttd-$VER-$1
+	patch -p 0 -d openttd-$VER-$1 < openttd-trunk-android.patch && patch -p 0 -d openttd-$VER-$1 < findversion.sh.patch || exit 1
 fi
 
-if [ \! -f openttd-$VER/objs/lang/english.lng ] ; then
-	sh -c "cd openttd-$VER && ./configure --without-freetype --without-png --without-zlib --without-lzo2 --without-lzma --endian=LE && make lang && make -C objs/release endian_target.h depend && make -C objs/setting"
-	rm -f openttd-$VER/Makefile
+if [ \! -f openttd-$VER-$1/objs/lang/english.lng ] ; then
+	sh -c "cd openttd-$VER-$1 && ./configure --without-freetype --without-png --without-zlib --without-lzo2 --without-lzma --endian=LE && make lang && make -C objs/release endian_target.h depend && make -C objs/setting"
+	rm -f openttd-$VER-$1/Makefile
 fi
 
-if [ \! -f openttd-$VER/Makefile ] ; then
-	../setEnvironment.sh sh -c "cd openttd-$VER && env ./configure --host=arm-linux-androideabi --with-sdl --with-freetype=sdl-config --with-png --with-zlib --without-icu --with-libtimidity=$LOCAL_PATH/../../../obj/local/armeabi/libtimidity.so --with-lzo2=$LOCAL_PATH/../../../obj/local/armeabi/liblzo2.so --prefix-dir='.' --data-dir='' --without-allegro --without-fontconfig --with-lzma --endian=LE || echo 'Run: sudo apt-get install liblzma-dev - it is needed only for configure script'"
+if [ \! -f openttd-$VER-$1/Makefile ] ; then
+	../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && env ./configure --host=$2 --with-sdl --with-freetype=sdl-config --with-png --with-zlib --without-icu --with-libtimidity=$LOCAL_PATH/../../../obj/local/armeabi/libtimidity.so --with-lzo2=$LOCAL_PATH/../../../obj/local/armeabi/liblzo2.so --prefix-dir='.' --data-dir='' --without-allegro --without-fontconfig --with-lzma --endian=LE || echo 'Run: sudo apt-get install liblzma-dev - it is needed only for configure script'"
 fi
-../setEnvironment.sh sh -c "cd openttd-$VER && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -llzo2 -lpng -ltimidity -lfreetype -lgcc -lz -lc -lgnustl_static -lsupc++'" && cp -f openttd-$VER/objs/release/openttd libapplication.so
+../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -llzo2 -lpng -ltimidity -lfreetype -lgcc -lz -lc -lgnustl_static -lsupc++'" && cp -f openttd-$VER-$1/objs/release/openttd libapplication-$1.so
