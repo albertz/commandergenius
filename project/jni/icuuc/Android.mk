716d3df64450ea4f265f69db93813110b1563aec
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-03-12 22:43:57 +0200

    Added ICU library

---
+++ project/jni/icuuc/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := $(notdir $(LOCAL_PATH))
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).so
+
+include $(PREBUILT_SHARED_LIBRARY)
---
+++ project/jni/icuuc/readme.txt
@@ -0,0 +1,3 @@
+To use ICU library, you will need to download language database into path
+share/icu/52.1/icudt52l.dat into the app current directory.
+Just unpack the provided archive icudt52l.zip and you're good to go.
