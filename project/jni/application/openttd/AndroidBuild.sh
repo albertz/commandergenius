a0ceb71cd94157b4fa3c4cddb7bc3a9a00205021
pelya <x.pelya.x@gmail.com>
2014-03-15 21:07:32 +0200

    OpenTTD: created separate Git repo for Android patches

--- project/jni/application/openttd/AndroidBuild.sh
+++ project/jni/application/openttd/AndroidBuild.sh
@@ -4,21 +4,16 @@ LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 VER=1.4.0-beta5
 
-if [ \! -d openttd-$VER-$1 ] ; then
-	[ -e openttd-$VER-source.tar.gz ] || wget http://binaries.openttd.org/releases/$VER/openttd-$VER-source.tar.gz || exit 1
-	tar xvzf openttd-$VER-source.tar.gz
-	cp -a -f openttd-$VER openttd-$VER-orig
-	mv -f openttd-$VER openttd-$VER-$1
-	patch -p 0 -d openttd-$VER-$1 < openttd-trunk-android.patch || exit 1
-fi
+[ -d openttd-$VER-$1 ] || mkdir -p openttd-$VER-$1/bin/baseset
 
-if [ \! -f openttd-$VER-$1/objs/lang/english.lng ] ; then
-	sh -c "cd openttd-$VER-$1 && ./configure --without-freetype --without-png --without-zlib --without-lzo2 --without-lzma --endian=LE && make lang && make -C objs/release endian_target.h depend && make -C objs/setting"
+[ -e openttd-$VER-$1/objs/lang/english.lng -o "$1" '!=' armeabi-v7a ] || {
+	sh -c "cd openttd-$VER-$1 && ../src/configure --without-freetype --without-png --without-zlib --without-lzo2 --without-lzma --endian=LE && make lang && make -C objs/release endian_target.h depend && make -C objs/setting" || exit 1
 	rm -f openttd-$VER-$1/Makefile
-fi
+} || exit 1
 
 export ARCH=$1
-if [ \! -f openttd-$VER-$1/Makefile ] ; then
-	../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && env ./configure --host=$2 --with-sdl --with-freetype=sdl-config --with-png --with-zlib --with-icu --with-libtimidity=$LOCAL_PATH/../../../obj/local/armeabi/libtimidity.so --with-lzo2=$LOCAL_PATH/../../../obj/local/armeabi/liblzo2.so --prefix-dir='.' --data-dir='' --without-allegro --with-fontconfig --with-lzma --endian=LE || echo 'Run: sudo apt-get install liblzma-dev - it is needed only for configure script'"
-fi
-../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -llzo2 -lpng -ltimidity -lfreetype -lfontconfig -lexpat -licui18n -liculx -licule -licuuc -licudata -lgcc -lz -lc -lgnustl_static -lsupc++'" && cp -f openttd-$VER-$1/objs/release/openttd libapplication-$1.so
+[ -e openttd-$VER-$1/Makefile ] || {
+	../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && env ../src/configure --host=$2 --with-sdl --with-freetype=sdl-config --with-png --with-zlib --with-icu --with-libtimidity=$LOCAL_PATH/../../../obj/local/$ARCH/libtimidity.so --with-lzo2=$LOCAL_PATH/../../../obj/local/$ARCH/liblzo2.so --prefix-dir='.' --data-dir='' --without-allegro --with-fontconfig --with-lzma --endian=LE || echo 'Run: sudo apt-get install liblzma-dev - it is needed only for configure script'"
+} || exit 1
+
+../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && make -j4 VERBOSE=1 STRIP='' LIBS='-lsdl-1.2 -llzo2 -lpng -ltimidity -lfreetype -lfontconfig -lexpat -licui18n -liculx -licule -licuuc -licudata -lgcc -lz -lc -lgnustl_static -lsupc++'" && cp -f openttd-$VER-$1/objs/release/openttd libapplication-$1.so || exit 1
