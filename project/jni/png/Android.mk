eb203c3cc734d80bd1c540433500b4129601ffc5
pelya <x.pelya.x@gmail.com>
2014-03-22 02:30:30 +0200

    Replaced PNG with version from AOSP sources, this fixed greyscale PNG not loading

--- project/jni/png/Android.mk
+++ project/jni/png/Android.mk
@@ -1,35 +1,30 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-common_SRC_FILES := \
-png.c \
-pngerror.c \
-pngget.c \
-pngmem.c \
-pngpread.c \
-pngread.c \
-pngrio.c \
-pngrtran.c \
-pngrutil.c \
-pngset.c \
-pngtest.c \
-pngtrans.c \
-pngwio.c \
-pngwrite.c \
-pngwtran.c \
-pngwutil.c \
+LOCAL_SRC_FILES := \
+	png.c \
+	pngerror.c \
+	pnggccrd.c \
+	pngget.c \
+	pngmem.c \
+	pngpread.c \
+	pngread.c \
+	pngrio.c \
+	pngrtran.c \
+	pngrutil.c \
+	pngset.c \
+	pngtrans.c \
+	pngvcrd.c \
+	pngwio.c \
+	pngwrite.c \
+	pngwtran.c \
+	pngwutil.c
 
-common_CFLAGS := -O3 -DHAVE_CONFIG_H
+LOCAL_CFLAGS := -std=gnu89 -fvisibility=hidden ## -fomit-frame-pointer -ftrapv
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_LDLIBS := z
 
-common_C_INCLUDES += $(LOCAL_PATH)/include $(LOCAL_PATH)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-#LOCAL_LDLIBS := -lz
-
-LOCAL_MODULE:= png
+LOCAL_MODULE := png
 
 include $(BUILD_STATIC_LIBRARY)
-
