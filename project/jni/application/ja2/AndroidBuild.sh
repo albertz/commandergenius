aa56c63aeb12b5f3e918feead01ae0a4f9d14d41
Sergii Pylypenko <x.pelya.x@gmail.com>
2014-10-13 22:44:27 +0300

    Updated JA2

--- project/jni/application/ja2/AndroidBuild.sh
+++ project/jni/application/ja2/AndroidBuild.sh
@@ -5,4 +5,4 @@
 	patch -p1 -d src-$1 < ja2.patch || exit 1
 } || exit 1
 
-../setEnvironment-$1.sh sh -c "make -j4 -C src-$1 WITH_LPTHREAD=0" && cp -f src-$1/ja2 libapplication-$1.so
+../setEnvironment-$1.sh sh -c "make -j4 -C src-$1 WITH_LPTHREAD=0 Q=" && cp -f src-$1/ja2 libapplication-$1.so
