c05fb4c4047d9ed0b1fde1968cdb0d711977c788
Sergii Pylypenko <x.pelya.x@gmail.com>
2015-06-03 23:23:37 +0300

    Updated SuperTux

--- project/jni/application/supertux/AndroidPreBuild.sh
+++ project/jni/application/supertux/AndroidPreBuild.sh
@@ -4,5 +4,5 @@ if [ -e supertux/patched.successfully ]; then
 	exit 0
 else
 	# Google Code fails to send shallow repository, so we're checking out everything
-	git clone https://code.google.com/p/supertux && patch -p1 -d supertux < android.diff && touch supertux/patched.successfully || exit 1
+	git clone https://github.com/SuperTux/supertux.git && patch -p1 -d supertux < android.diff && touch supertux/patched.successfully || exit 1
 fi
