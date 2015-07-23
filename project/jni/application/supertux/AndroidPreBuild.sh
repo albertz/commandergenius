b33a34b59d6f6715146f45cf1680e8062405045e
pelya <x.pelya.x@gmail.com>
2015-07-23 21:13:20 +0300

    SuperTux: another broken symlink

--- project/jni/application/supertux/AndroidPreBuild.sh
+++ project/jni/application/supertux/AndroidPreBuild.sh
@@ -6,4 +6,5 @@ else
 	# Google Code fails to send shallow repository, so we're checking out everything
 	git clone https://github.com/SuperTux/supertux.git && patch -p1 -d supertux < android.diff && touch supertux/patched.successfully || exit 1
 	ln -sf supertux/data/images/engine/icons/supertux-256x256.png icon.png
+	ln -sf ../supertux/data/images/engine/menu/logo.png AndroidData/logo.png
 fi
