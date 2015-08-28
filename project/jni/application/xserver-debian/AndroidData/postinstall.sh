1f3c5dadf40d33a361107b6b027eeb1434a742da
Sergii Pylypenko <x.pelya.x@gmail.com>
2015-08-28 20:11:20 +0300

    Debian and GIMP: fixed unpacking files, broken on 64-bit architectures.

--- project/jni/application/xserver-debian/AndroidData/postinstall.sh
+++ project/jni/application/xserver-debian/AndroidData/postinstall.sh
@@ -5,6 +5,32 @@ cd $SECURE_STORAGE_DIR
 ./busybox tar xzf $UNSECURE_STORAGE_DIR/data-1.tar.gz
 rm -f $UNSECURE_STORAGE_DIR/data-1.tar.gz
 ARCH=`getprop ro.product.cpu.abi`
+echo "Detected architecture $ARCH"
+# We only support armeabi-v7a and x86
+case $ARCH in
+	armeabi-v7a)
+		;;
+	x86)
+		;;
+	arm64-v8a)
+		ARCH=armeabi-v7a
+		;;
+	arm64_v8a)
+		ARCH=armeabi-v7a
+		;;
+	x86_64)
+		ARCH=x86
+		;;
+	x86-64)
+		ARCH=x86
+		;;
+	*)
+		echo "=== ERROR ERROR ERROR ==="
+		echo "Unsupported architecture: $ARCH"
+		sleep 10000
+		exit 1
+		;;
+esac
 echo "Copying files for architecture $ARCH"
 ./busybox cp -af img-$ARCH/. img/
 echo "Removing files for other architectures"
