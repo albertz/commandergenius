48bb326ed976369854703b290e903a762d18bc9b
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-01-11 20:33:10 +0200

    SuperTux: bump version to remove the app from Cardboard-specific list

--- project/jni/application/supertux/AndroidPreBuild.sh
+++ project/jni/application/supertux/AndroidPreBuild.sh
@@ -3,7 +3,7 @@
 if [ -e supertux/patched.successfully ]; then
 	exit 0
 else
-	git clone https://github.com/SuperTux/supertux.git || exit 1
+	git clone --depth 1 --branch release/0.4 https://github.com/SuperTux/supertux.git || exit 1
 	git -C supertux submodule update --init || exit 1
 	patch -p1 -d supertux < android.diff && touch supertux/patched.successfully || exit 1
 	ln -sf supertux/data/images/engine/icons/supertux-256x256.png icon.png
