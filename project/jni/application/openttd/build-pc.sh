4df7e9dae99e64360dee851fe7a1bb86d3b12975
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-05-23 21:42:24 +0300

    Updated TODO

--- project/jni/application/openttd/build-pc.sh
+++ project/jni/application/openttd/build-pc.sh
@@ -18,7 +18,7 @@ cd bin
 export LD_LIBRARY_PATH=/usr/local/lib
 
 if [ -z "$1" ]; then
-	./openttd -d 2 -r 854x480
+	./openttd -d 2 -r 854x480 -g opntitle.sav
 else
 	gdb -ex run --args \
 	./openttd -d 2 -r 854x480 -g opntitle.sav
