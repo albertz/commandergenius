088fde5d5155baea020e5e1745e3cbc823c92516
Sergii Pylypenko <x.pelya.x@gmail.com>
2016-06-03 22:00:25 +0300

    SDL: updated the script to check static symbol size in the library

--- checkStaticDataSize.sh
+++ checkStaticDataSize.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Output static/global symbols in a application lib to check if someone allocated 200 Mb static temp array.
 # Used to resolve linker errors such as: Cannot load library: alloc_mem_region[815]: OOPS:    54 cannot map library 'libapplication.so'. no vspace available.
-objdump -x project/obj/local/armeabi/libapplication.so | grep '	' | sed 's/.*	/0x/' | sort -n | xargs -n 2 printf '%d %s\n'
+OBJ="$1"
+[ -z "$OBJ" ] && OBJ=project/obj/local/armeabi-v7a/libapplication.so
+objdump -x -C -w "$OBJ" | grep '	' | sed 's/.*	/0x/' | sort -n | while read SIZE NAME ; do printf '%8d ' "$SIZE" ; echo "$NAME" ; done
