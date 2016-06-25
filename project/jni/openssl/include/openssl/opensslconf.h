3a2d1525738f962da9196d980d6f6b54c0b63dcd
Gerhard Stein <stein@leat.rub.de>
2016-06-25 18:13:43 +0200

    Banner for Commander Genius

--- project/jni/openssl/include/openssl/opensslconf.h
+++ project/jni/openssl/include/openssl/opensslconf.h
@@ -118,8 +118,8 @@ extern "C" {
 
 #if !(defined(VMS) || defined(__VMS)) /* VMS uses logical names instead */
 #if defined(HEADER_CRYPTLIB_H) && !defined(OPENSSLDIR)
-#define ENGINESDIR "/home/pelya/src/endless_space/SDL-android/project/jni/openssl/build/armeabi-v7a/dist/lib/engines"
-#define OPENSSLDIR "/home/pelya/src/endless_space/SDL-android/project/jni/openssl/build/armeabi-v7a/dist/ssl"
+#define ENGINESDIR "/home/gerstrong/workspace/SDLAndroid/project/jni/openssl/build/armeabi-v7a/dist/lib/engines"
+#define OPENSSLDIR "/home/gerstrong/workspace/SDLAndroid/project/jni/openssl/build/armeabi-v7a/dist/ssl"
 #endif
 #endif
 
