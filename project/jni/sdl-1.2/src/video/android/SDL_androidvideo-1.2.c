6cd731dceef8e23b50308c5519c20776a87a0873
lubomyr <lubomyr31@gmail.com>
2016-08-19 19:54:22 +0300

    added 720x400 virtual wide screen mode

--- project/jni/sdl-1.2/src/video/android/SDL_androidvideo-1.2.c
+++ project/jni/sdl-1.2/src/video/android/SDL_androidvideo-1.2.c
@@ -130,7 +130,7 @@ int ANDROID_ToggleFullScreen(_THIS, int fullscreen)
 	return 1;
 }
 
-enum { SDL_NUMMODES = 58 };
+enum { SDL_NUMMODES = 59 };
 static SDL_Rect *SDL_modelist[SDL_NUMMODES+1];
 
 //#define SDL_modelist		(this->hidden->SDL_modelist)
@@ -342,7 +342,8 @@ int ANDROID_VideoInit(_THIS, SDL_PixelFormat *vformat)
 	SDL_modelist[55]->w = 1280; SDL_modelist[55]->h = 960; // For UQM-HD
 	SDL_modelist[56]->w = 960; SDL_modelist[56]->h = 600; // For ScummVM 3x-mode
 	SDL_modelist[57]->w = 960; SDL_modelist[57]->h = 540; // Virtual wide-screen mode
-	SDL_modelist[58] = NULL;
+	SDL_modelist[58]->w = 720; SDL_modelist[58]->h = 400; // Virtual wide-screen mode
+	SDL_modelist[59] = NULL;
 	// If you going to add another video mode, increase SDL_NUMMODES constant
 	
 	SDL_VideoInit_1_3(NULL, 0);
