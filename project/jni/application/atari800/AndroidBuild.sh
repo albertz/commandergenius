05d9653643f81c87e0e9c5c73e81a70c273a2178
lubomyr <lubomyr31@gmail.com>
2016-08-27 22:28:35 +0300

    updated atari800 port

--- project/jni/application/atari800/AndroidBuild.sh
+++ project/jni/application/atari800/AndroidBuild.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-
 LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
-ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi/libSDL.so
+JOBS=1
 
-if [ \! -f atari800/configure ] ; then
-	sh -c "cd atari800/src && ./autogen.sh"
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/$1/libSDL.so
+
+if [ \! -f atari800/src/configure ] ; then
+	env sh -c "cd atari800/src && ./autogen.sh"
 fi
 
 if [ \! -f atari800/src/Makefile ] ; then
-	../setEnvironment.sh sh -c "cd atari800/src && ./configure --build=x86_64-unknown-linux-gnu --host=arm-linux-androideabi --target=sdl --without-x"
+	../setEnvironment-$1.sh sh -c "cd atari800/src && ./configure --build=x86_64-unknown-linux-gnu --host=$2 --target=default --without-x --enable-onscreenkeyboard"
 fi
 
-make -C atari800/src && mv -f atari800/src/atari800 libapplication.so
+make -C atari800/src -j$JOBS && mv -f atari800/src/atari800 libapplication-$1.so
