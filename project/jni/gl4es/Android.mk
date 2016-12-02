45f514b823f6e7c5b616f22c20723f70cdfa81c9
lubomyr <lubomyr31@gmail.com>
2016-12-03 00:51:12 +0200

    gl4es updated, added latest changes by ptitSeb

--- project/jni/gl4es/Android.mk
+++ project/jni/gl4es/Android.mk
@@ -51,6 +51,7 @@ LOCAL_SRC_FILES := \
 	src/glx/streaming.c \
 
 LOCAL_CFLAGS += -g -std=c99 -funwind-tables -O3 -DBCMHOST -fvisibility=hidden -include include/android_debug.h
+#LOCAL_CFLAGS += -DNO_INIT_CONSTRUCTOR
 
 #LOCAL_LDLIBS := -ldl -llog
 
